17375980d2a6c09477fe0b1fc6a3e058f05101a0
Kettukaa <33789207+Kettukaa@users.noreply.github.com>
2022-02-11 01:32:30 -0500

    Added ability to control the initial x

--- pytorch_diffusion/noise.py
+++ pytorch_diffusion/noise.py
@@ -35,28 +35,37 @@ def slerp(v0, v1, t, DOT_THRESHHOLD=0.9995):
     v2 = s0 * v0_copy + s1 * v1_copy
     return v2
 
+
 class Noise:
-    def __init__(self, shape, n, seeds=None, interp_func=slerp, state_files=('gs1.pt','gs2.pt'), device='cuda'):
+    def __init__(self, shape, n, seeds=None, interp_func=slerp, filenames=('gs1.pt','gs2.pt','x0.pt'), device='cuda'):
         self.shape = shape
         self.n = n
         self.g1 = torch.Generator(device=device)
         self.g2 = torch.Generator(device=device)
         self.state1 = None
         self.state2 = None
+        self.x0 = None 
+
         self.device = device
         self.interp_func = interp_func
 
+
+
         # If no seeds are provided it is implied that a state is being loaded
         if seeds is None:
-            print(f'No seed specified, loading states: {state_files}')
-            self.load_states(*state_files)
-
+            print(f'No seed specified, loading states: {filenames}')
+            self.load_states(*filenames[:-1])
+            self.load_x0(filenames[-1])
         # Otherwise, make, save, and load a new state file
         else:
-            print('Seeds specified, generating state files...')
+            print('Seeds specified, generating state files and x0...')
             print(f'Shape:{self.shape}\t Seeds:{seeds}\t total states per seed:{n}')
-            self.make_states(seeds, n, state_files, self.shape)
-            self.load_states(*state_files)
+            x0_seed = seeds.pop()
+            self.make_states(seeds, n, filenames, self.shape)
+            self.load_states(*filenames[:-1])
+            self.make_x0(x0_seed, filename=filenames[-1], size=self.shape)
+            self.load_x0(filenames[-1])
+
         print()
 
     def interp(self, n, bs, interp_func=slerp):
@@ -90,11 +99,23 @@ class Noise:
             torch.save(states, filename)
         print(f'States saved as {filenames}')
 
+    def make_x0(self, seed, filename='x0.pt', *args, **kwargs):
+        g = torch.Generator(device= self.device)
+        g.manual_seed(seed)
+        self.x0 = torch.randn(*args, **kwargs, generator=g, device=self.device).unsqueeze(0)        
+        torch.save(self.x0, filename)
+        print(f'x0 saved as {filename}')
+
     def load_states(self, file1, file2):
         '''Loads states from filenames'''
         self.state1 = torch.load(file1)
         self.state2 = torch.load(file2)
         print(f'loaded states {file1} {file2}')
+
+    def load_x0(self, file):
+        '''Loads the initial x0 from file'''
+        self.x0 = torch.load(file)
+        print(f'loaded x0 {file}')
 
     def set_states(self, n):
         '''Sets the state of both generators to the nth respective state'''
@@ -105,7 +126,7 @@ class Noise:
         '''Returns a two element list containing both noise tensors at the nth state'''
         self.set_states(n)
         return torch.stack([torch.randn(self.shape, generator=g, device=self.device) for g in [self.g1, self.g2]])
-    
+
     def repeat(self, n, m):
         '''Returns m copies of x1 in state n '''
         return self.randn(n)[0].unsqueeze(0).repeat((m, 1,1,1))
